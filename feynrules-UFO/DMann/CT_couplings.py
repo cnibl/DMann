2a5f5701a7e09de7de5a1839ab731c06fefc3bc2
Carl Niblaeus <carl.niblaeus@fysik.su.se>
2018-11-16 17:27:24 +0100

    Added feynrules model files to make UFO files for MG.

---
+++ feynrules-UFO/DMann/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.31
+# Mathematica version: 11.0.0 for Mac OS X x86 (64-bit) (July 28, 2016)
+# Date: Fri 16 Nov 2018 17:25:52
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ feynrules-UFO/DMann/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.3.31
+# Mathematica version: 11.0.0 for Mac OS X x86 (64-bit) (July 28, 2016)
+# Date: Fri 16 Nov 2018 17:25:52
+
+
+from object_library import all_orders, CouplingOrder
+
+
+DMS = CouplingOrder(name = 'DMS',
+                    expansion_order = 2,
+                    hierarchy = 2)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
