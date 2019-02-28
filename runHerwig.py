ab655dc42203d27ef1b2943944273bba656176a5
Carl Niblaeus <carl.niblaeus@fysik.su.se>
2019-02-28 18:22:11 +0100

    Changes to runHerwig script, now fully working.

--- runHerwig.py
+++ runHerwig.py
@@ -10,11 +10,12 @@ Creates .in-files for Herwig for the combinations in simSettings.py and then run
 import os
 import subprocess
 import sys
-import multiprocessing as mp 
-import simSettings as sets 
 import time
 import math
-
+import glob
+import errno
+import multiprocessing as mp 
+import simSettings as sets 
 
 def get_abspath(path):
    """
@@ -25,19 +26,27 @@ def get_abspath(path):
    else:
       return os.path.abspath(path)
 
-def run_herwig(infile,outDir,nAnn,annCh,sun=False):
+def run_herwig(infile,nAnn,annCh,sun=False):
 	"""
 	Runs Herwig on the provided infile and puts results for the run in outDir.
 	Input: infile - a .in-file to be run as "Herwig read infile"
-	       outDir - the directory to put results for this run in
 	       nAnn - number of annihilations
 	       annCh - the annihilation channel
 	       sun - whether to set up for the sun or not (default is halo)
-	Returns: path to infile, error code
+	Returns: path to infile, error code, time spent (s)
 	"""
+	start=time.time()
+	if os.path.split(os.getcwd())[1]!="Herwig":
+		os.chdir(os.path.join(os.getcwd(),"Herwig"))
+	with open(infile[:-3]+".log","w") as log:
+		print "\tStarting %s" % (os.path.split(infile)[1])
+		p=subprocess.Popen(["Herwig","read",infile],stdout=log,stderr=log)
+		returnCode=p.wait()
+	end=time.time()
 
+	return (infile,returnCode,end-start)
 
-def write_infile(nAnn,outDir,mwimp,annCh,sun):
+def write_infile(nAnn,fileDir,resDir,mwimp,annCh,sun):
 	"""
 	Writes the infile for input arguments
 	Input: outDir - the directory to put results for this run in
@@ -47,9 +56,9 @@ def write_infile(nAnn,outDir,mwimp,annCh,sun):
 	Returns: path to infile
 	"""
 	if sun==False:
-		fileName=outDir+"da-her7-m"+str(mwimp)+"-"+annCh+"-halo.in"
+		fileName=os.path.join(fileDir,"da-her7-m"+str(mwimp)+"-"+annCh+"-halo.in")
 	else:
-		fileName=outDir+"da-her7-m"+str(mwimp)+"-"+annCh+"-sun.in"
+		fileName=os.path.join(fileDir,"da-her7-m"+str(mwimp)+"-"+annCh+"-sun.in")
 	
 	with open(fileName,"w") as f:
 		f.write("# "+fileName+"\n# -*- ThePEG-repository -*-\n")
@@ -204,8 +213,8 @@ def write_infile(nAnn,outDir,mwimp,annCh,sun):
 		f.write("set DMannYields:MassDM "+str(mwimp)+"\n")
 		f.write("set DMannYields:NEvent "+str(nAnn)+"\n")
 		f.write("set DMannYields:AnnState "+annCh+"\n")
-		if len(outDir)>0:
-			f.write("set DMannYields:OutDir "+outDir+"\n")
+		if len(resDir)>0:
+			f.write("set DMannYields:OutDir "+resDir+"\n")
 		f.write("insert DMannYields:ParticleCodes[0] -12\n")
 		f.write("insert DMannYields:ParticleCodes[0] 12\n")
 		f.write("insert DMannYields:ParticleCodes[0] -14\n")
@@ -220,21 +229,72 @@ def write_infile(nAnn,outDir,mwimp,annCh,sun):
 		f.write("do /Herwig/MatrixElements/Matchbox/Factory:ProductionMode\n")
 		f.write("cd /Herwig/Generators\n")
 		f.write("set EventGenerator:NumberOfEvents "+str(nAnn)+"\n")
-		f.write("run "+fileName+" EventGenerator\n")
+		f.write("run "+os.path.split(fileName)[1][:-3]+" EventGenerator\n")
 	return fileName
 
-def mk_outdir():
-	pass
 
+def mkdir_p(path):
+   """
+   Creates directory path only if nonexistent.
+   """
+   try:
+      os.makedirs(path)
+   except OSError as exc:  # Python >2.5
+      if exc.errno == errno.EEXIST and os.path.isdir(path):
+         pass
+      else:
+         raise
+
+def collect_result(res):
+   """
+   A callback function needed to collect results in apply_async call 
+   """
+   global results
+   results.append(res)
+
+# The threshold in GeV that the WIMP mass has to exceed for annihilations to be possible
+annThresholds={"WLWL" : 80.4, "WTWT" : 80.4, "ZLZL" : 91.2, "ZTZT" : 91.2, "hh" : 125.2, 
+               "taLtaL" : 1.8, "taRtaR" : 1.8, "muLmuL" : 0.11, "muRmuR" : 0.11, "ee" : 5.2e-6, 
+               "tLtL" : 173., "tRtR" : 173., "bb" : 4.8, "cc" : 1.3, "ss" : 0.1, "uu" : 2.6e-3, "dd" : 5.1e-3}
 
 
 if __name__=="__main__":
-	"Do stuff"
-	pass
+	"Create Herwig .in-files from all combinations in simSettings and run through them in parallell."
+	
+	print "Starting Herwig7 runs ..."
+	allstart=time.time()
+	infileDir=get_abspath(os.path.join(os.getcwd(),"Herwig","Runs_"+sets.RUN_TAG))
+	try:
+		os.makedirs(infileDir)
+	except OSError as exc: #if directory exists, clear it of old in-files
+		if exc.errno==errno.EEXIST and os.path.isdir(infileDir):
+			oldFiles=glob.glob(os.path.join(infileDir,"*.in"))
+			delCmd=["rm","-f"]
+			for f in oldFiles:
+				delCmd.append(f)
+			subprocess.call(delCmd)
 
-	#chdir(./Herwig)
-	#for combinations in simSettings:
-	#   create in-file
-	#   apply_async(run_her,args=(),callback=)
-	#pool.close
-	#pool.join 
+	pool=mp.Pool(processes=mp.cpu_count())
+	results=[]
+	for annCh in sets.ANN_CHANNELS:
+		for mWIMP in sets.WIMP_MASSES:
+			if annThresholds[annCh] < mWIMP:
+				resDir=os.path.join(get_abspath(sets.DMANN_OUT_DIR),"Herwig",annCh+"-m"+str(mWIMP))
+				mkdir_p(resDir)
+				infilePath=get_abspath(write_infile(sets.N_ANN,infileDir,resDir,mWIMP,annCh,False))
+				pool.apply_async(run_herwig,args=(infilePath,sets.N_ANN,annCh,False),callback=collect_result)
+			else:
+				print "\tWarning: m_WIMP = %5.3f GeV too small for annihilation channel %s, skipping" % (mWIMP,annCh)
+         	continue # skip to next mwimp value
+	pool.close()
+	pool.join()	
+	allend=time.time()
+	print "Done! Total time: %i h, %i min, %i s" % (int(math.floor(math.floor((allend-allstart)/60)/60)),
+	                                                          int(math.floor((allend-allstart)/60)%60),
+	                                                          int((allend-allstart)%60))
+
+	print "Have run Herwig7 on following files:"
+	print "%-30s%-19s%-14s" % ("in-file","Return code","Time spent [s]")
+	for infile,ret,delta in results:
+		print "%-35s\t%-2i\t%15.1f" % (os.path.relpath(infile,start=get_abspath(os.path.join(os.getcwd(),"Herwig",infileDir))),ret,delta)
+
---
+++ runHerwig.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/carlniblaeus/DMann/Herwig
+make all
+cd /home/carlniblaeus/DMann
+source activate py27 #must be before Herwig activation!
+source /home/carlniblaeus/Herwig7/bin/activate
+python runHerwig.py 
+
